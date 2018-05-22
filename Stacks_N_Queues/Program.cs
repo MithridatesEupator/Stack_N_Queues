c1075c30beee440e696d80329b92a86afd546c19
MithridatesEupator <fsaulean@gmail.com>
2018-05-21 20:14:00 -0400

    added queue

--- Stacks_N_Queues/Program.cs
+++ Stacks_N_Queues/Program.cs
@@ -6,12 +6,8 @@ using System.Threading.Tasks;
 
 namespace Stacks_N_Queues
 {
-    class Stacks 
+    public class Stacks
     {
-
-        private Object[] stackArray;
-        private int stackSize;
-        private int topEntry = -1;
 
         public class Arrays
         {
@@ -24,11 +20,15 @@ namespace Stacks_N_Queues
             }
         }
 
-        Stacks(int size)
+        private Object[] stackArray;
+        private int stackSize;
+        private int topEntry = -1;
+
+        public Stacks(int size)
         {
             this.stackSize = size;
             stackArray = new String[size];
-            
+
         }
 
         public void push(Object input)
@@ -41,7 +41,7 @@ namespace Stacks_N_Queues
             }
             else
             {
-                Console.WriteLine("Error: Stack is Full\n");
+                Console.WriteLine("Error: Stack is full\n");
             }
         }
 
@@ -50,8 +50,8 @@ namespace Stacks_N_Queues
             if (topEntry >= 0)
             {
                 Object pop = stackArray[topEntry];
-                stackArray[topEntry] = null;
                 Console.WriteLine(pop + " was popped \n");
+                stackArray[topEntry] = null;
                 topEntry--;
                 return pop;
             }
@@ -62,7 +62,7 @@ namespace Stacks_N_Queues
         }
 
         public void displayStack()
-        {   
+        {
             Console.WriteLine("\n--------[DISPLAY STACK]--------");
             foreach (object item in stackArray)
             {
@@ -72,18 +72,89 @@ namespace Stacks_N_Queues
                 }
             }
             Console.WriteLine("-------------------------------");
+        }   
+    }
+
+    public class Queue
+    {
+
+        Object[] queueArray;
+        int queueSize = -1;
+
+        public Queue(int size)
+        {
+            queueArray = new Object[size];
         }
 
-        static void Main()
+        public void insert(Object entry)
         {
-            Stacks test = new Stacks(6);
-            test.push("10");
-            test.push("22");
-            test.push("33");
-            test.push("42");
-            var t = test.pop();
+            if (queueSize < queueArray.Length)
+            {
+                Console.WriteLine(entry + " was inserted \n");
+                queueSize++;
+                queueArray[queueSize] = entry;
+            }
+            else
+            {
+                Console.WriteLine("Error: Queue is full \n");
+            }
+        }
+
+        public void delete()
+        {
+            if (queueSize != -1)
+            {
+                Object[] tempArray = (Object[])queueArray.Clone();
+                Console.WriteLine(queueArray[queueSize] + " was deleted \n");
+                queueArray[queueSize] = null;
+                
+                for(int i = queueSize; i != 0; i--)
+                {
+                    queueArray[i - 1] = tempArray[i];
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Queue is empty \n");
+            }
+        }
+
+        public void displayQueue()
+        {
+            Console.WriteLine("\n-------[DISPLAY QUEUE]-------");
+            foreach (object item in queueArray)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            Console.WriteLine("-----------------------------");
+        }
+
+        
+    }
+    class FinalClass
+    {
+    
+        public static void Main()
+        {
+            Queue queueExample = new Queue(6);
+            queueExample.insert("10");
+            queueExample.insert("22");
+            queueExample.insert("33");
+            queueExample.insert("42");
+            queueExample.delete();
+            queueExample.displayQueue();
+            //===========================================
+            Stacks stackExample = new Stacks(6);
+            stackExample.push("10");
+            stackExample.push("22");
+            stackExample.push("33");
+            stackExample.push("42");
+            var t = stackExample.pop();
             Console.WriteLine(t);
-            test.displayStack();
+            stackExample.displayStack();
         }
     }
 }
